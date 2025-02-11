6f20b27d68d47266855e5d27355a8530efe0706b
asafdarltu23 <asafdar@ltu.edu>
2025-02-11 16:18:45 -0500

    Popup System Finished
    Redid it to make it easier to create popup windows as prefabs to put into the manager script. Added more functions to spawn popups. Added a bool to check if popups should be able to be spawned or not. Made popups instantiated instead of enabled or disabled.

---
+++ Assets/Scripts/PopupWindowScripts/PopupParametersToButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+/*This script is necessary to call functions with multiple parameters while still being able to easily input values in inspector
+I don't know how else to do this so this is a temporary solution*/
+
+public class PopupParametersToButton : MonoBehaviour
+{
+    GameObject manager;
+
+    [Header("Function: chanceSpecificPopup(chance, listElement)")]
+    [Tooltip("Parameter 1")]
+    public int chance;
+    [Tooltip("Paramter 2")]
+    public int listElement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        manager = GameObject.Find("PopupsManager");
+    }
+
+    public void chanceSpecificPopup()
+    {
+        if (Random.Range(0, 100) <= chance)
+            manager.GetComponent<PopupSpawnManageScript>().spawnPopup(listElement);
+    }
+}
---
+++ Assets/Scripts/PopupWindowScripts/PopupSpawnManageScript.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+// ==> THIS SCRIPT TO BE ATTATCHED TO AN EMPTY GAMEOBJECT NAMED "PopupsManager" <==
+public class PopupSpawnManageScript : MonoBehaviour
+{
+    //Use this in other scripts to disable or enable popups
+    public bool allowPopups = true;
+
+    //Boundaries
+    float minX = 0f;
+    float maxX = 1f - 0.25f;
+    float minY = 0f;
+    float maxY = 1f - 0.25f;
+
+    public Camera cam;
+
+    public List<GameObject> popupWindows;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        if (cam == null)
+            print("Camera not found");
+    }
+
+    public void spawnRandomPopup()
+    {
+        if (allowPopups == true)
+        {
+            //Generate random position vector without going beyond camera view
+            float randX = Random.Range(minX, maxX);
+            float randY = Random.Range(minY, maxY);
+
+            Vector3 worldPos = cam.ViewportToWorldPoint(new Vector3(randX, randY, cam.nearClipPlane));
+
+            //Randomly chooses then instantiates a popup at the random position
+            int randPopup = Random.Range(0, popupWindows.Count);
+            Instantiate(popupWindows[randPopup], new Vector3(worldPos.x, worldPos.y, this.gameObject.transform.position.z), Quaternion.identity);
+        }
+    }
+
+    public void chancePopup(float chance)
+    {
+        if (Random.Range(0, 100) <= chance)
+            spawnRandomPopup();
+    }
+
+    public void spawnPopup(int listPos)
+    {
+        if (allowPopups == true)
+        {
+            //Generate random position vector without going beyond camera view
+            float randX = Random.Range(minX, maxX);
+            float randY = Random.Range(minY, maxY);
+
+            Vector3 worldPos = cam.ViewportToWorldPoint(new Vector3(randX, randY, cam.nearClipPlane));
+
+            //Instantiate the specific popup chosen from the list
+            Instantiate(popupWindows[listPos], new Vector3(worldPos.x, worldPos.y, this.gameObject.transform.position.z), Quaternion.identity);
+        }
+    }
+}
+
